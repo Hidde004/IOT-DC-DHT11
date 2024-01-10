79d1c91529ec5bf42b4d76b5888880dfd9edc171
Hidde004 <148006908+Hidde004@users.noreply.github.com>
2024-01-10 11:31:49 +0100

    Update _DHT-11.met.DC-motor.ino v1.1

--- _DHT-11.met.DC-motor.ino
+++ _DHT-11.met.DC-motor.ino
@@ -1,3 +1,15 @@
+#include <WiFiS3.h>
+#include <ArduinoMqttClient.h>
+
+char ssid[] = "IoTatelierF2144";
+char pass[] = "IoTatelier";
+WiFiClient wifiClient;
+MqttClient mqttClient(wifiClient);
+const char broker[] = "192.168.144.1";
+const char topic[] = "hiddezijlstra/Temp";
+const int port = 1883;
+const char publishTopic[] = "hiddezijlstra/Temp";
+
 #include <DHT.h>
 
 #define DHTPIN 5    
@@ -13,6 +25,22 @@
 DHT dht(DHTPIN, DHTTYPE);
 
 void setup() {
+  while (WiFi.begin(ssid, pass) != WL_CONNECTED) {
+    delay(5000);
+  }
+  
+  bool MQTTconnected = false;
+  while (!MQTTconnected) {
+    if (!mqttClient.connect(broker, port))
+      delay(1);
+    else
+      MQTTconnected = true;
+    }
+    
+    mqttClient.onMessage(onMqttMessage);
+    mqttClient.subscribe(topic);
+    Serial.println(topic);
+
   Serial.begin(9600);
   pinMode(motorIN1, OUTPUT);
   pinMode(motorIN2, OUTPUT);
@@ -43,6 +71,12 @@ void loop() {
   } else {
     Serial.println("Failed to read temperature from DHT sensor!");
   }
+
+  mqttClient.beginMessage("hiddezijlstra/Temp");
+  mqttClient.print(temperature);
+  mqttClient.endMessage();
+
+  mqttClient.poll();
 }
 
 void startMotor() {
@@ -66,3 +100,14 @@ void turnOnGreenLED() {
   digitalWrite(redPin, LOW);
   digitalWrite(greenPin, HIGH);
 }
+
+  void onMqttMessage(int messageSize) 
+  {
+  while (mqttClient.available()) 
+  {
+    Serial.print((char)mqttClient.read());
+    }
+    
+  Serial.println();
+  Serial.println();
+  }
